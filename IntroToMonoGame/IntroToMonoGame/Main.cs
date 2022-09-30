7aa46057c5e28cc02013cea46f416f421de38bff
Niall McGuinness <niall.mcguinness@dkit.ie>
2022-09-30 12:51:09 +0100

    Adding indexbuffer concept

--- IntroToMonoGame/IntroToMonoGame/Main.cs
+++ IntroToMonoGame/IntroToMonoGame/Main.cs
@@ -38,6 +38,196 @@ namespace GD
             //vertices using a specific vertex type
             vertices = new VertexPositionColor[]
                 {
+                };
+
+            //world
+            rotZ = 0;
+            world = Matrix.Identity * Matrix.CreateRotationZ(rotZ);
+
+            //view
+            view = Matrix.CreateLookAt(new Vector3(0, 2, 2),
+                Vector3.Zero, Vector3.UnitY);
+
+            //projection
+            projection = Matrix.CreatePerspectiveFieldOfView(
+                MathHelper.PiOver2, 640.0f / 480, 0.1f, 100);
+
+            //effect
+            effect = new BasicEffect(_graphics.GraphicsDevice);
+            effect.VertexColorEnabled = true;
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+    }
+}
+
+/*
+ namespace GD
+{
+    public class Main : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private VertexPositionColor[] vertices;
+        private float rotZ;
+        private Matrix world;
+        private Matrix view;
+        private Matrix projection;
+        private BasicEffect effect;
+
+        public Main()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        private void SetGraphics(
+            int width, int height, bool isMouseVisible)
+        {
+            //calling set property
+            _graphics.PreferredBackBufferWidth = width;
+            _graphics.PreferredBackBufferHeight = height;
+            IsMouseVisible = isMouseVisible;
+            _graphics.ApplyChanges();
+        }
+
+        protected override void Initialize()
+        {
+            SetGraphics(1024, 768, false);
+
+            //vertices using a specific vertex type
+            vertices = new VertexPositionColor[]
+                {
+                    //FL
+                    new VertexPositionColor(new Vector3(-1,0,1), Color.Red),
+                    //FR
+                    new VertexPositionColor(new Vector3(1,0,1), Color.Green),
+                    //BR
+                    new VertexPositionColor(new Vector3(1,0,-1), Color.Blue),
+                     //BL
+                    new VertexPositionColor(new Vector3(-1,0,-1), Color.Yellow),
+                    //FL
+                    new VertexPositionColor(new Vector3(-1,0,1), Color.Red),
+
+                    //FLU
+                    new VertexPositionColor(new Vector3(0,1,0), Color.Black),
+
+                    //BRU
+                    new VertexPositionColor(new Vector3(1,0,-1), Color.White),
+                };
+
+            //world
+            rotZ = 0;
+            world = Matrix.Identity * Matrix.CreateRotationZ(rotZ);
+
+            //view
+            view = Matrix.CreateLookAt(new Vector3(0, 2, 2),
+                Vector3.Zero, Vector3.UnitY);
+
+            //projection
+            projection = Matrix.CreatePerspectiveFieldOfView(
+                MathHelper.PiOver2, 640.0f / 480, 0.1f, 100);
+
+            //effect
+            effect = new BasicEffect(_graphics.GraphicsDevice);
+            effect.VertexColorEnabled = true;
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            //    System.Diagnostics.Debug.WriteLine("LoadContent...");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            //rotZ += MathHelper.ToRadians(1);
+            //world = Matrix.Identity
+            //    //  * Matrix.CreateRotationX(rotZ / 2)
+            //    * Matrix.CreateRotationY(rotZ);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            effect.World = world;
+            effect.View = view;
+            effect.Projection = projection;
+
+            //setting the W, V, P variables for the draw frame
+            effect.CurrentTechnique.Passes[0].Apply();
+
+            //pass vertices
+            _graphics.GraphicsDevice.
+                DrawUserPrimitives<VertexPositionColor>(
+                PrimitiveType.LineStrip, vertices, 0, 6);
+
+            base.Draw(gameTime);
+        }
+    }
+}
+ */
+
+/*
+ namespace GD
+{
+    public class Main : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private VertexPositionColor[] vertices;
+        private float rotZ;
+        private Matrix world;
+        private Matrix view;
+        private Matrix projection;
+        private BasicEffect effect;
+
+        public Main()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        private void SetGraphics(
+            int width, int height, bool isMouseVisible)
+        {
+            //calling set property
+            _graphics.PreferredBackBufferWidth = width;
+            _graphics.PreferredBackBufferHeight = height;
+            IsMouseVisible = isMouseVisible;
+            _graphics.ApplyChanges();
+        }
+
+        protected override void Initialize()
+        {
+            SetGraphics(1024, 768, false);
+
+            //vertices using a specific vertex type
+            vertices = new VertexPositionColor[]
+                {
                     //pyramid base - front
                     new VertexPositionColor(new Vector3(-1,0,1), Color.Red),
                     new VertexPositionColor(new Vector3(1,0,1), Color.Red),
@@ -129,6 +319,8 @@ namespace GD
         }
     }
 }
+
+ */
 
 /*
  namespace GD
